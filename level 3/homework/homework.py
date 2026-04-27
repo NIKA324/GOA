ac9cc7f0e92a0958ef0f123d4867958055e0fae2
NIKA324 <nikakupa55@gmail.com>
2026-04-27 22:08:35 +0400

    commit

---
+++ level 3/homework/homework.py
@@ -0,0 +1,55 @@
+#momxmareblistvis misalmeba
+
+#name = input ("nika:")
+#print ("gamarjoba " + name + "!")
+
+#ricxvis gaormageba
+
+#number = int( 5 )
+#print(number * 2)
+
+#matematikuri operatorebi 5 magaliti
+
+#num1 = int(( 2 ))
+#print ( num1 - num2 )
+
+#print ( num1 * num2 )
+
+#print ( num1 / num2 )
+
+#print ( num1 ** num2 )
+
+# ori adamianis dialogi
+
+#person1 = input("enter name")
+#person2 = input("enter name")
+
+#print("gamarjoba ioane:" , person1)
+#print("gagimarjos saba:" , person2)
+
+#ra aris input
+
+#input funqcia gamoiyeneba momxmareblisgan informaciis misagebad.
+# is acherebs programis mushaobas manam, sanam momxmarebeli rames ar chawers da ar daawers enter-s.
+#mnishvnelovania: input yoveltvis abrunebs teqstur (string) monacems.
+
+# user_data = input("salami:")
+
+# int() funqcia gamoiyeneba monacemis mtel ricxvad gadasaqcevad.
+# radgan input () yoveltvis teqsts gvadzlevs, chven gvwirdeba int()
+# rata shevdzlot matematikuri gamotvlebis chatareba cifrebze.
+
+# age = int(input(17)) #teqsti iqceva ricxvad
+
+# ra aris str()
+
+#str() funqcia gamoiyeneba nebismieru monacemis tipis (magalitad ricxvis)
+#teqstad (string) gadaasaqcevad.
+#es mosaxerxebelia, roca gvinda ricxvi da teqsti ertmanets mivawebot.
+
+points = 100
+
+message = "tqven gaqtvt" + str(points) + "qula."
+
+print(message)
+
---
+++ level 4/classwork.py
@@ -0,0 +1,15 @@
+#1
+#wins = int(input())
+ties = int(input())
+
+points = wins * ties * 0.5
+
+#2
+bill = int(input())
+
+tip = bill * 0.2 
+print(tip)
+
+bill = int(input())
+tip = bill * 0.2
+print(tip)
